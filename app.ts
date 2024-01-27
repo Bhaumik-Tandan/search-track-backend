a3e124270f3a9cf481f2856c0d62cd0caa3eb1b2
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-27 20:33:12 +0530

    [add] redirectlion working

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import connectDB from './src/config/db';
 import passport from 'passport';
 import authentication from "./src/routes/Authentication";
+import cookieSession from 'cookie-session';
 
 connectDB();
 
@@ -11,6 +12,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
+app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: ["key"] }))
 app.use(passport.initialize());
 app.use(passport.session());
 
