126c9a654b25d6578038e80b9898f014a6b5205d
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-27 18:35:12 +0530

    [ADD] auth

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
-import cookieSession from 'cookie-session';
 import connectDB from './config/db';
 import passport from 'passport';
+import authentication from "./routes/Authentication";
 
 dotenv.config({ path: './.env' });
 
@@ -12,11 +12,9 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [process.env.COOKIE_KEY||""] }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-const authentication = require('./src/routes/authentication');
 
 app.use('/api/v1/auth', authentication);
 
