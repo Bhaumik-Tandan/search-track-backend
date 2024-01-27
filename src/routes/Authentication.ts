bb304e331a3021d5ad5de047241f952ab0fc4bfb
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-27 22:43:32 +0530

    [fix] CODE

--- src/routes/Authentication.ts
+++ src/routes/Authentication.ts
@@ -8,4 +8,22 @@ router.get('/google', passport.authenticate('google', { scope: ['profile', 'emai
 router.get('/google/callback', passport.authenticate('google', {
   successRedirect: 'http://localhost:3000/dashboard',
 }));
+
+router.get('/google/callback', passport.authenticate('google', {
+  successRedirect: 'http://localhost:3000/dashboard',
+}));
+router.get('/me', (req, res) => {
+  res.send(req.user);
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  console.log('logging out');
+  req.logout((err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
+    res.redirect('/');
+  });
+});
+
 export default router;
