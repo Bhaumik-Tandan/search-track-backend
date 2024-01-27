a98e4d022efb5d2531cdc27de7ddb200a986fdd0
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-28 00:09:16 +0530

    [add] code

--- src/routes/Authentication.ts
+++ src/routes/Authentication.ts
@@ -10,14 +10,13 @@ router.get('/google/callback', passport.authenticate('google', {
 }));
 
 router.get('/google/callback', passport.authenticate('google', {
-  successRedirect: 'http://localhost:3000/dashboard',
+  successRedirect: 'http://localhost:3000',
 }));
 router.get('/me', (req, res) => {
   res.send(req.user);
 });
 
 router.get('/logout', (req: Request, res: Response) => {
-  console.log('logging out');
   req.logout((err) => {
     if (err) {
       return res.status(500).json({ error: err.message });
