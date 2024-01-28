5110fef168ad043d1cb56698926fd5ed3d8a4652
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-28 20:18:23 +0530

    [add] cors header sin callback

--- src/routes/Authentication.ts
+++ src/routes/Authentication.ts
@@ -5,9 +5,34 @@ import "../services/Passport";
 const router = express.Router();
 
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/google/callback', passport.authenticate('google', {
-  successRedirect: process.env.FRONTEND_URI,
-}));
+router.get('/google/callback', (req, res, next) => {
+  // Always set the Access-Control headers
+
+  // Allow specified methods if specified in the request
+  res.setHeader(
+    "access-control-allow-methods",
+    req.headers["access-control-request-method"] || "*"
+  );
+
+  // Allow specified headers if specified in the request
+  res.setHeader(
+    "access-control-allow-headers",
+    req.headers["access-control-request-headers"] || "*"
+  );
+
+  // Allow the requesting origin and set credentials to true
+  if (req.headers.origin) {
+    res.setHeader("access-control-allow-origin", req.headers.origin);
+    res.setHeader("access-control-allow-credentials", "true");
+  }
+
+  // Continue with the Google OAuth authentication using Passport
+  passport.authenticate('google', {
+    successRedirect: process.env.FRONTEND_URI,
+  })(req, res, next);
+});
+
+
 
 router.get('/me', (req, res) => {
   res.send(req.user);
