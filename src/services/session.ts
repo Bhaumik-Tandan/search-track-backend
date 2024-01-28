99adbaad5884061c29f1de9f1ce6760af2be756c
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-01-28 15:24:06 +0530

    [add] code for cookie

--- src/services/session.ts
+++ src/services/session.ts
@@ -1,13 +1,13 @@
-import session from 'express-session';
+import session, { SessionOptions } from 'express-session';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sessionOptions = {
+const sessionOptions: SessionOptions = {
   secret: process.env.SESSION_SECRET || '',
   resave: false,
   saveUninitialized: false,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000,sameSite: false },
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, sameSite: 'none', secure: true },
 };
 
 export default session(sessionOptions);
